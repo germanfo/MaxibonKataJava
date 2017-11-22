215679d4bfafc35ef787a8634bd0f98aba7c366d
German Franco <german.franco@vass.es>
2017-11-22 16:08:06 +0100

    * First Property Test

---
+++ src/test/java/com/karumi/maxibonkata/DeveloperProperties.java
@@ -0,0 +1,30 @@
+package com.karumi.maxibonkata;
+
+import com.pholser.junit.quickcheck.From;
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.generator.java.lang.IntegerGenerator;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+import static junit.framework.TestCase.assertTrue;
+
+@RunWith(JUnitQuickcheck.class)
+public class DeveloperProperties {
+
+    private Developer developer;
+
+    @Before
+    public void setUp(){
+
+    }
+
+    //@From(IntegerGenerator.class) NO ES NECESARIO PONERLO PORQUE ES UN TIPO PRIMITIVO
+    @Property public void theNumberOfMaxibonsToGrabShouldNeverBeNegative(@From(IntegerGenerator.class) int numberOfMaxibonsToGrab){
+        developer = new Developer("PEPE", numberOfMaxibonsToGrab);
+
+        System.out.println(numberOfMaxibonsToGrab);
+        assertTrue(developer.getNumberOfMaxibonsToGrab() >= 0);
+
+    }
+}
